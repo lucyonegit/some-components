b6128d1c4b6a74b0848833c4a0a5bba65bc25882
{ lucyonegit } <{ 18629049410@163.com }>
2018-02-08 16:26:45 +0800

    newchange

--- src/components/pcenter.vue
+++ src/components/pcenter.vue
@@ -1,3 +1,54 @@
 <template>
-  <p>我是个人中心</p>
+  <div>
+      <div class="tt">
+        <div class="main">
+              <img src="../image/pcenter/Smile.png" alt="userimg" id="s1">
+              <p>您还没有登录呢</p>
+              <img src="../image/pcenter/more.png" alt="more" id="s2">
+        </div>
+      </div>
+  </div>
 </template>
+<script>
+export default {
+  name:"pcenter"
+}
+</script>
+<style scoped>
+.tt{
+  width: 100%;
+  height: 2.5rem;
+  background-color: rgb(66, 169, 230);
+   display: flex;
+  flex-direction: row;
+  align-items: center;
+ 
+}
+.main{
+  width: 95%;
+  height: 1rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+}
+#s1{
+  width: .8rem;
+  height: .8rem;
+  border-radius: 100%;
+
+}
+.tt p{
+  font-size: .25rem;
+  margin-left: -1.2rem;
+  color: white;
+}
+#s2{
+  width: .4rem;
+  height: .4rem;
+
+  border-radius: 100%;
+}
+
+</style>
